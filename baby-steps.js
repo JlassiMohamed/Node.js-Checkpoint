87e06dd4b5a7cffa7be3a4748cf3b7c1a2be739f
mohamed <inj.jlassi@gmail.com>
2021-03-07 22:27:20 +0100

    full complete version

--- baby-steps.js
+++ baby-steps.js
@@ -1,3 +1,8 @@
+/*  BABY STEPS (Exercise 2 of 13)   
+  Write a program that accepts one or more numbers as command-line arguments  
+  and prints the sum of those numbers to the console (stdout).
+*/
+
 let s = 0;
 for (let i = 2; i <= process.argv.length - 1; i++) {
   s = s + Number(process.argv[i]);
